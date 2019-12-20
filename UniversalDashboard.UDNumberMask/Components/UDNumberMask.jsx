a9b4f397cfb143fb14d1df60f21989ae9310c24e
Adam <adambacon1@hotmail.com>
2019-12-20 22:35:04 +0000

    Add files via upload

---
+++ UniversalDashboard.UDNumberMask/Components/UDNumberMask.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import NumberFormat from 'react-number-format';
+
+class UDNumberMask extends React.Component {
+  render() {
+    return (
+      <NumberFormat format={this.props.format}
+        mask={this.props.mask}
+        thousandSeparator={this.props.thousandSeparator}
+        prefix={this.props.prefix}
+        placeholder={this.props.placeholder}
+        displayType={this.props.displayType}
+        value={this.props.value}
+      />
+    )
+  }
+}
+
+export default UDNumberMask
---
+++ UniversalDashboard.UDNumberMask/Components/index.js
@@ -0,0 +1,2 @@
+import UDNumberMask from './UDNumberMask';
+UniversalDashboard.register("UD-NumberMask", UDNumberMask);
